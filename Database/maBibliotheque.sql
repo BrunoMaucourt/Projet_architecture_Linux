6c316e7114497cfd86a92d920e586b8141e353f7
Bruno <mct.bruno@gmail.com>
2023-02-03 09:34:23 +0100

    screenchots add

--- Database/maBibliotheque.sql
+++ Database/maBibliotheque.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:8889
--- Généré le : mar. 31 jan. 2023 à 15:23
+-- Généré le : jeu. 02 fév. 2023 à 10:29
 -- Version du serveur : 5.7.34
 -- Version de PHP : 8.0.8
 
@@ -20,57 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `maBibliotheque`
 --
-
--- --------------------------------------------------------
-
---
--- Structure de la table `auteur`
---
-
-CREATE TABLE `auteur` (
-  `ID` int(11) NOT NULL,
-  `Nom` varchar(256) NOT NULL,
-  `Prenom` varchar(256) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `catergorie`
---
-
-CREATE TABLE `catergorie` (
-  `ID` int(11) NOT NULL,
-  `Categorie` varchar(256) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `edition`
---
-
-CREATE TABLE `edition` (
-  `ID` int(11) NOT NULL,
-  `Edition` varchar(256) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `livre`
---
-
-CREATE TABLE `livre` (
-  `ID` int(11) NOT NULL,
-  `ID_Titre` int(11) NOT NULL,
-  `ID_Auteur` int(11) NOT NULL,
-  `ID_Categorie` int(11) NOT NULL,
-  `SynopsisEditeur` text NOT NULL,
-  `DateParution` date NOT NULL,
-  `ID_Edition` int(11) NOT NULL,
-  `Stock` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -92,46 +41,13 @@ CREATE TABLE `maBibliotheque` (
 --
 
 INSERT INTO `maBibliotheque` (`ID`, `nomLivre`, `nomAuteur`, `categorie`, `datePublication`, `synopsis`) VALUES
-(3, 'Au guet !', 'Terry Pratchett', 'Fantasy', 1997, 'Une société secrète d’encagoulés complote pour renverser le seigneur Vétérini, Patricien d’Ankh-Morpork, et lui substituer un roi.\r\n            C’est sans compter avec le guet municipal et son équipe de fins limiers.\r\n            Une affaire à la mesure du capitaine Vimaire – s’il boit, c’est pour oublier les laideurs de la vie – et de ses brillants adjoints. (« Tous pour un ! » )\r\n            Et lorsqu’on retrouve au petit jour dans les rues les corps des citoyens transformés en biscuits calcinés, l’enquête s’oriente résolument vers un dragon de vingt-cinq mètres qui crache le feu ; on aurait quelques questions à lui poser.\r\n            Mais peut-être la collaboration du bibliothécaire de l’Université ne sera-t-elle pas inutile : n’arbore-t-il pas aussi une plaque de la DST (Défense simienne du territoire) ?');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `titre`
---
-
-CREATE TABLE `titre` (
-  `ID` int(11) NOT NULL,
-  `Titre` varchar(256) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+(1, 'Au guet !', 'Terry Pratchett', 'Fantasy', 1997, 'Une société secrète d’encagoulés complote pour renverser le seigneur Vétérini, Patricien d’Ankh-Morpork, et lui substituer un roi.\r\nC’est sans compter avec le guet municipal et son équipe de fins limiers.\r\nUne affaire à la mesure du capitaine Vimaire – s’il boit, c’est pour oublier les laideurs de la vie – et de ses brillants adjoints. (« Tous pour un ! » )\r\nEt lorsqu’on retrouve au petit jour dans les rues les corps des citoyens transformés en biscuits calcinés, l’enquête s’oriente résolument vers un dragon de vingt-cinq mètres qui crache le feu ; on aurait quelques questions à lui poser.\r\nMais peut-être la collaboration du bibliothécaire de l’Université ne sera-t-elle pas inutile : n’arbore-t-il pas aussi une plaque de la DST (Défense simienne du territoire) ?'),
+(2, 'Ronde de nuit ', 'Terry Pratchett', 'Fantasy', 2011, 'C\'est un homme comblé que le duc Sam Vimaire, commissaire divisionnaire du Guet d\'Ankh-Morpork, heureux père bientôt. Hélas ! la poursuite d\'un dangereux criminel entraîne un accident qui le ramène dans son propre passé, en un temps de tumulte et de violence.\r\nVivre dans le passé n\'est pas facile mais y mourir étonnamment simple. Il doit pourtant survivre car des tâches essentielles l\'attendent : mettre le grapin sur un meurtrier, s\'instruire lui-même, débutant, pour devenir un bon flic et canger l\'issue d\'une rébellion sanglante.\r\nÀ l\'assaut des paradoxes temporels, un \"conte d\'une ville\" façon Disque-monde, avec sa collection de gavroches, de dames à l\'affection négociable (\"L\'amour au juste prix !\"), de rebelles, de policiers de la Secrète et autres enfants de la révolution.\r\nAjouter une citation'),
+(3, 'Coup de Tabac ', 'Terry Pratchett', 'Fantasy', 2011, 'Blousé, Vimaire. Dame Sybil, son épouse aimante, lui impose quinze jours de congés à la campagne dans le manoir familial. La vie de hobereau, rien de folichon pour le commissaire divisionnaire du Guet d Ankh-Morpork, non plus que la déférence servile qu on lui témoigne, à lui qui tient pour article de foi que les hommes sont tous les mêmes le pantalon baissé. La tradition le veut, le flic en vacances n a pas ouvert sa valise que le premier cadavre lui saute à la figure. Mais ce n est pas un meurtre ordinaire qui attend Vimaire, c est un crime contre l existence et la dignité d une espèce entière. Qu importe s il est hors de sa juridiction, si les repères lui manquent dans le monde rural et si l on s acharne à le mener en bateau, la justice doit passer.');
 
 --
 -- Index pour les tables déchargées
 --
-
---
--- Index pour la table `auteur`
---
-ALTER TABLE `auteur`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Index pour la table `catergorie`
---
-ALTER TABLE `catergorie`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Index pour la table `edition`
---
-ALTER TABLE `edition`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Index pour la table `livre`
---
-ALTER TABLE `livre`
-  ADD PRIMARY KEY (`ID`);
 
 --
 -- Index pour la table `maBibliotheque`
@@ -140,50 +56,14 @@ ALTER TABLE `maBibliotheque`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Index pour la table `titre`
---
-ALTER TABLE `titre`
-  ADD PRIMARY KEY (`ID`);
-
---
 -- AUTO_INCREMENT pour les tables déchargées
 --
-
---
--- AUTO_INCREMENT pour la table `auteur`
---
-ALTER TABLE `auteur`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `catergorie`
---
-ALTER TABLE `catergorie`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `edition`
---
-ALTER TABLE `edition`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `livre`
---
-ALTER TABLE `livre`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `maBibliotheque`
 --
 ALTER TABLE `maBibliotheque`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT pour la table `titre`
---
-ALTER TABLE `titre`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 ## Contexte du projet
 
-Projet réalisé dans le cadre du cours Architecture Linux, à la Coding-Factory. Le but de ce module formation était de créer un environnement de production via deux machines virtuelles sous Debian, en mode CLI. L'une servant de serveur Apache2 / PHP et la deuxième de serveur MariaDB / MySQL. Le résultat est observé un client HTTP (navigateur Web) de la machine hôte. Les machines virtuelles sont crées sur MacOs avec UTM.
+Projet réalisé dans le cadre du cours Architecture Linux, à la Coding-Factory. Le but de ce module de formation était de créer un environnement de production via deux machines virtuelles sous Debian, en mode CLI. L'une servant de serveur Apache2 / PHP et la deuxième de serveur MariaDB / MySQL. Le résultat est observé avec un client HTTP (navigateur Web) de la machine hôte. Les machines virtuelles sont crées sur Mac OS avec le logiciel UTM.
 
-Le site herbergé est un simple CRUD sur le thème d'une bibliothèque (front-end boostrap et back-end PHP / MySQL).
+Le site herbergé sur ce serveur de production est un simple CRUD sur le thème d'une bibliothèque (front-end boostrap et back-end PHP / MySQL).
 
 Une base de données test se trouve aussi sur ce repository Git ainsi que de la documentation pour installer les machines virtuelles.
 
